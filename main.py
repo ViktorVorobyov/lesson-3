a017f9e241d9caffc3d2e27aff5f15d11230006f
ViktorVorobyov <tmspython@gmail.com>
2021-07-18 22:59:30 +0300

    Homework done

---
+++ main.py
@@ -0,0 +1,12 @@
+x = 2 + 2 * ((2 - 2) / 2)
+print (x)
+
+x = (2 + (2 * 2 - 2)) / 2
+print(x)
+
+money = 2130 * (1.1**5 - 1) + 2130 + (5 * 120)
+print(money)
+
+my_list = ['s', 'o', 'l', 'u', 't', 'i', 'o', 'n']
+my_string = "".join(my_list[::-1])
+print(my_string)
---
+++ solution-03.py
@@ -0,0 +1,3 @@
+my_list = ['s', 'o', 'l', 'u', 't', 'i', 'o', 'n']
+my_string = "".join(my_list[::-1])
+print(my_string)
